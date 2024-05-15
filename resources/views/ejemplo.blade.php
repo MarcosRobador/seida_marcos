f7a510981d1cc82c4b5b83beabbade83e14fcf3c
Marcos <Marcoscabet@gmail.com>
2024-05-15 14:13:04 +0200

    vistas

--- resources/views/ejemplo.blade.php
+++ resources/views/ejemplo.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Página de Inicio')
 
 @section('content')
+    <img src="{{ asset('images/fondo.png') }}" alt="Imagen de Inicio" style="width:100%;">
     <h1>Bienvenido a la página de inicio</h1>
     <p>Este es un ejemplo de contenido que se inserta en la plantilla base.</p>
 @endsection
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -6,34 +6,37 @@
     <title>@yield('title', 'App Name')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <header class="bg-dark text-white">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark">
-                <a class="navbar-brand" href="{{ url('/') }}">App Name</a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="height: 40px;"> 
+                    Zapatillini
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Inicio</a>
+                            <a class="nav-link active" aria-current="page" href="{{ url('/') }}"><i class="fas fa-home"></i> Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/about') }}">Acerca de</a>
+                            <a class="nav-link" href="{{ url('/about') }}"><i class="fas fa-user"></i> Acerca de</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/services') }}">Servicios</a>
+                            <a class="nav-link" href="{{ url('/services') }}"><i class="fas fa-briefcase"></i> Servicios</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/contact') }}">Contacto</a>
+                            <a class="nav-link" href="{{ url('/contact') }}"><i class="fas fa-envelope"></i> Contacto</a>
                         </li>
                     </ul>
                 </div>
-            </nav>
-        </div>
-    </header>
+                
+    </header>    
 
     <main class="py-4">
         @yield('content')
